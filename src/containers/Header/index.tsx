2504731e4a96708955e375a7d4e07d9c5e9676a4
GuntharDeNiro <gunthardeniro@gmail.com>
2020-10-04 20:53:47 +0200

    Update index.tsx

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -128,7 +128,7 @@ class Head extends React.Component<any> {
                 background: "#E0A300"
               },
         };
-        const marketToggle = {"background": "#E0A300"}
+        const marketToggle = {color:"#000","background": "#80E0A300"}
         return [
             <div style={marketToggle} className="pg-header__market-selector-toggle" onClick={this.props.toggleMarketSelector}>
                 <p className="pg-header__market-selector-toggle-value">
